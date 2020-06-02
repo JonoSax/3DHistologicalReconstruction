c3338f212aca505a0ce7065e438c6339c432e632
Jonathan Reshef <jono.reshef@gmail.com>
2020-06-02 21:18:24 +1200

    processes files individually + comments for work

--- main.py
+++ main.py
@@ -5,25 +5,42 @@ segment identification
 '''
 
 from HelperFunctions import *
-import glob
+from glob import glob
+
+# ---------- THINGS TO DO ----------
+# Make it so that the directories of the slices and the annotated slices are all in a single callable object, rather than seperate variables
 
 # User input
 '''
 Folder location of slices
 Extent of training (epochs, batch)
 '''
-
+# data directory
 data = '/Users/jonathanreshef/Documents/2020/Masters/TestingStuff/Segmentation/Data.nosync/testing/'
 
+kernel = 150
+
 # NOTE functions need to be able to deal with a list of names
-slicesDIr = glob(str(data+"*.ndpi"))
+slicesDir = glob(str(data+"*.ndpi"))
+
+test = slicesDir[0].split(data)[-1].split(".ndpi") 
 annotationsDir = slices = glob(str(data+"*.ndpa"))
 
-# Load in the WSI segments
-segments = WSILoad.main(slicesDir)
+# get a list of all the names of the ndpi and ndpa files
+names = list()
+for file in glob(data + "*.ndpi"): 
+    names.append(file.split(data)[-1].split(".ndpi")[0])           
 
-# Load in the locations of identified slices
-annotations = SegmentLoad.readndpa(annotationsDir)
+SegmentLoaded = list()
+WSILoaded = list()
+
+for name in names:
+
+    # Load in the locations of identified slices
+    SegmentLoaded.append(SegmentLoad.readndpa(data, name))
+
+    # Load in the WSI segments, seperate into target tissue and non-target
+    WSILoaded.append(WSILoad.main(kernel, data, name, 0, annotations))
 
 # NOTE this could possible go into the WSILoad function
 # Perform pre-processing on the WSI to highlight features/remove background+artifacts 
