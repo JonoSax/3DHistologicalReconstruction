d31a9e370e96901435510f157c4772d21a4d01b7
Jonathan Reshef <jono.reshef@gmail.com>
2020-06-02 13:32:17 +1200

    loads in all ndpi files and segments them per kernel size into tif

--- HelperFunctions/DataGenerator.py
+++ HelperFunctions/DataGenerator.py
@@ -10,53 +10,92 @@ import tifffile as tifi
 import cv2
 from skimage.measure import block_reduce
 from PIL import Image
-import glob
+from glob import glob
 
-kernel = 150
-imageSRC =  "TestImage20.tif"
+# kernel = 150
+# imageSRC =  "Data.nosync/testing/"
+# size = 0
 
-def main(kernel, imageSRC):
+def main(kernel, imageSRC, size):
 
     # This function segments the tif image into n x n images 
-    # Input:    Square kernel size (pixels)
-    #           image source directory            
-    # Output:   a directory which contains the segmented sections, each 
+    # Input:    (kernel), Square kernel size (pixels)
+    #           (imageSRC), ndpi source director
+    #           (size), scale image to be extracted from the ndpi file            
+    # Output:   (dir), a list of directories which contains the segmented sections, each 
     #           named according to their segmented position    
 
-    # type = input("CV2 or PIL processing: ")
-    # TO DO: Make this a glob function so that a common name set of the images can be processed (from NDPA)
+    # get all the npdi files of interest
+    imagesNDPI = glob(imageSRC + "*.ndpi")
 
-    # NOTE need to make this dependent on the image input
-    outlines = "H653A_9.ndpi.ndpa"
+    # convert ndpi images into tif files of set size
+    ndpiLoad(size, imagesNDPI)
 
-    # find the image by name, agnostic of img type
-    # imageDir = glob.glob(image+"*")
+    # get the name of the tif files extracted
+    imagesTIF = glob(imageSRC + "*.tif")
 
-    # converting to string
+    dirs = list()
 
-    # size of the square kernel used to segment image
+    # for each ndpi image found, convert into a tif and segment
+    for imageTIF in imagesTIF:
 
-    img = tifi.imread(imageDir)
+        # read the tif file into a numpy array
+        img = tifi.imread(imageTIF)
 
-    height, width, channels = img.shape
-    print(height, width, channels)
+        # get tif dimenstions --> necessary for the segmentation
+        height, width, channels = img.shape
+
+        # create a directory containing the segmentations of the imager
+        dir = str(imageTIF + "_tiles")
+        dirs.append(dir)
+
+        try:
+            os.mkdir(dir)
+        except OSError:
+            print("\nReplacing existing files\n")
+
+        # create the number of indexes for segmentation 
+        acr = int(height / kernel)
+        up = int(width / kernel)
+
+        # segment tissue
+        for h in range(acr):
+            for w in range(up):
+                segment = img[0 + kernel * h:kernel + kernel * h, 0 + kernel * w:kernel + kernel * w, :]
+                # print(image+"_tiles/Segment_"+str(h)+"_"+str(w)+"_"+str(image))
+                cv2.imwrite(imageTIF + "_tiles/Segment_" + str(h) + "_" + str(w) + ".tif", segment)
+                print("height = " + str(h) + "/" + str(up) + ", width = " + str(w) + "/" + str(acr))
+
+        print(imageTIF + " done")
+
+    print("done all")  
+
+    return(dirs)
 
 
-    try:
-        os.mkdir(image + "_tiles")
-    except OSError:
-        print("\nReplacing existing files\n")
 
-    acr = int(height / kernel)
-    up = int(width / kernel)
+def ndpiLoad(i, src):
 
-    # segment tissue
-    for h in range(acr):
-        for w in range(up):
-            segment = img[0 + kernel * h:kernel + kernel * h, 0 + kernel * w:kernel + kernel * w, :]
-            # print(image+"_tiles/Segment_"+str(h)+"_"+str(w)+"_"+str(image))
-            cv2.imwrite(image + "_tiles/Segment_" + str(h) + "_" + str(w) + "_" + str(imageDir), segment)
-            print("height = " + str(h) + "/" + str(up) + ", width = " + str(w) + "/" + +str(acr))
+    # This function extracts tif files from the raw ndpi files. This uses the 
+    # ndpitool from https://www.imnc.in2p3.fr/pagesperso/deroulers/software/ndpitools/ 
+    # Install as necessary. 
+    # Input:    (i), magnificataion level to be extracted from the ndpi file
+    #           options are 0.15625, 0.3125, 0.625, 1.25, 2.5, 5, 10, 20
+    #           (src), list of files to be extracted with set magnification
+    # Output:   (), tif file of set magnification, saved in the same directory
+    #           (), the tif files extracted is renamed to be simplified
+    #           as just [name]_[magnification].tif
 
+    mag = [0.15625, 0.3125, 0.625, 1.25, 2.5, 5, 10, 20][i]
 
-    print("done")
+    for s in src:
+
+        os.system("ndpisplit -x" + str(mag) + " " + str(s))
+
+        nameSRC = s.split("/")[-1].split(".")[0]                    # photo name
+        dirSRC = s.split(nameSRC + ".ndpi")[0]                      # folder of photo
+
+        extractedName = glob(s.split(".ndpi")[0] + "*z0.tif")[0]    # NOTE, use of z0 is to prevent 
+                                                                    # duplication of the same file, however 
+                                                                    # if there is z shift then this will fail
+        os.rename(extractedName, dirSRC + nameSRC + "_" + str(mag) + ".tif")
