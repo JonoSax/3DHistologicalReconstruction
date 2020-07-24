ee3dcde761e60a37878ebc9856d7b4314c4c92dc
Jonathan Reshef <jono.reshef@gmail.com>
2020-07-24 15:24:50 +1200

    significantly improved the ability to classify images according to specimens, 0 missing images now

--- tif2pdf.py
+++ tif2pdf.py
@@ -9,6 +9,9 @@ import numpy as np
 from glob import glob
 import os
 import img2pdf as i2p
+from multiprocessing import Process
+from time import perf_counter as clock
+
 
 def nameFromPath(paths):
     # this function extracts the name from a path
@@ -38,10 +41,86 @@ def nameFromPath(paths):
 
     return names
 
+# create temporary jpg file of all the tif images
+def pdfCreator(sampleCollections, s):
+    # this function takes the directory names and creates a pdf of each sample 
+    # Inputs:   (sampleCollections), dictionary containing all the dir names of the imgs
+    #           (s), specimen of interest
+    # Outputs:  (), creates a pdf per sample, also has to create a temporary folder
+    #               for jpeg images but that is deleted at the end
 
-# research drive access via VPN
-dataHome = '/Volumes/resabi201900003-uterine-vasculature-marsden135/Boyd collection/ConvertedNDPI/'
-# dataHome = '/eresearch/uterine/jres129/Boyd collection/ConvertedNDPI/'
+    # create a temporary folder for the jpeg images per sample
+    dataTemp = dataHome + 'temporary' + s + '/'
+    try:
+        os.mkdir(dataTemp)
+    except:
+        pass
+
+    specificSample = sampleCollections[s]
+
+
+    # order the dictionary values 
+    order = list(specificSample.keys())
+    orderS = [o.split()[0] for o in order]
+    orderN = np.array([''.join(i for i in o if i.isdigit()) for o in orderS]).astype(int)
+    orderI = np.argsort(orderN)
+
+    order = np.array(order)[orderI]
+    dirStore = list()
+
+    # create an ordered list of the sample directories
+    c = 0   # count for user to observe progress
+
+    startTime = clock()
+    for n in order:
+
+        print("Specimen: " + s + ", Sample " + str(c) + "/" + str(len(order)))
+        # load in the tif files and create a scaled down version
+        imgt = tifi.imread(specificSample[n])
+        scale = imgt.shape[1]/imgt.shape[0]
+        img = cv2.resize(imgt, (int(1000*scale), 1000))
+
+        # add the sample name to the image (top left corner)
+        cv2.putText(img, spec + "_" + str(n), 
+            (50, 50), 
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            1,
+            (0, 0, 0),
+            2)
+        # create a temporary jpg image and store the dir
+        tempName = dataTemp + spec + str(n) + '.jpg'
+        cv2.imwrite(tempName, img)
+        dirStore.append(tempName)
+        c += 1
+
+        endTime = clock()
+
+        timeOfProcess = endTime - startTime
+
+        timeLeft = timeOfProcess/c * (len(order) - c)
+
+        if c%5 == 0:
+            print("     Sample " + s + " has " + str(timeLeft) +  " secs to go")
+    
+    # comment the above loop and uncomment below to process already processed images
+    # without having to re-process all images
+    # dirStore = sorted(glob(dataTemp + spec + "*.jpg"))
+
+    # combine all the sample images to create a single pdf 
+    with open(dataPDF + s + ".pdf","wb") as f:
+        f.write(i2p.convert(dirStore))
+    print("PDF writing complete for " + s + "!\n")
+    # remove the temporary jpg files
+    for d in dirStore:
+        os.remove(d)
+
+    # remove the temporary dir
+    os.rmdir(dataTemp)
+
+    # research drive access via VPN
+
+# dataHome = '/Volumes/resabi201900003-uterine-vasculature-marsden135/Boyd collection/ConvertedNDPI/'
+dataHome = '/eresearch/uterine/jres129/Boyd collection/ConvertedNDPI/'
 
 samples = glob(dataHome + "*.tif")
 
@@ -52,13 +131,15 @@ specimens = list()
 for s in samples:
     spec, no = nameFromPath(s).split("_")
 
+    # attempt to get samples
     try:
-        no = int(no)
+        no = (no)
     except:
         # NOTE create a txt file of these files
         print("sample " + spec + no + " is not processed")
         continue
 
+    # create the dictionary as you go
     try:
         sampleCollections[spec][no] = s
     except:
@@ -72,48 +153,15 @@ try:
 except:
     pass
 
-dataTemp = dataHome + 'temporary/'
-try:
-    os.mkdir(dataTemp)
-except:
-    pass
+samples = list(sampleCollections.keys())
 
-# create temporary jpg file of all the tif images
-for s in sampleCollections:
-    print("Creating " + s)
-    order = np.sort(list(sampleCollections[s].keys()))
-    dirStore = list()
+# specific samples
+samples = ['H710C', 'H710A', 'H653A']
 
-    # create an ordered list of the sample directories
-    for n in order:
-        print("     Sample " + str(n) + "/" + str(np.max(order)))
+for s in samples:
+    Process(target=pdfCreator, args=(sampleCollections, s)).start()
 
-        # load in the tif files and create a scaled down version
-        imgt = tifi.imread(sampleCollections[s][n])
-        scale = imgt.shape[1]/imgt.shape[0]
-        img = cv2.resize(img, (int(1000*scale), 1000))
-
-        # add the sample name to the image (top left corner)
-        cv2.putText(img, spec + "_" + str(n), 
-            (50, 50), 
-            cv2.FONT_HERSHEY_SIMPLEX, 
-            1,
-            (0, 0, 0),
-            2)
-
-        # create a temporary jpg image and store the dir
-        tempName = dataTemp + spec + str(n) + '.jpg'
-        cv2.imwrite(tempName, img)
-        dirStore.append(tempName)
-
-    # combine all the sample images to create a single pdf 
-    print("Writing PDF")
-    with open(dataPDF + s + ".pdf","wb") as f:
-        f.write(i2p.convert(dirStore))
-    pritn("PDF writing complete!\n")
-    # remove the temporary jpg files
-    for d in dirStore:
-        os.remove(d)
-
-# remove the temporary dir
-os.rmdir(dataTemp)
+'''
+for s in samples:
+    pdfCreator(sampleCollections, s)
+'''
