d0a4ebaa0e476c9d6a858f722fc98fd5802736c6
Jonathan Reshef <jono.reshef@gmail.com>
2020-07-30 21:23:15 +1200

    now propogates features and multiprocesses

--- HelperFunctions/SegmentExtraction.py
+++ HelperFunctions/SegmentExtraction.py
@@ -13,104 +13,140 @@ from multiprocessing import Process
 
 def extract(dataTrain, name, size):
 
-    # This function reads in the .segSect files and aligned tif files
-    # and extracts the feature
+    # This function propogates any identifies features through all the samples
+    # and then extracts them
 
     # get the file of the features information 
     dirAligned = dataTrain + str(size) + '/alignedSamples/' + name
     dirSection = dataTrain + str(size) + '/sections/'
     dataAlignedTif = sorted(glob(dirAligned + '*.tif'))
 
-    # collect all the seg sections and if there is more than one then get the largest
-    # dimensions of all of them
     # NOTE this is pretty stupid but the origin for these co-ordinates is th
     # left (ie visual origin) instead of the actualy numpy origin (top left)
-    segSectionsALL = {}
-    segSectionsALL['seg0'] = (sorted(glob(dirAligned + name + '*.segsection')))
-    segSectionsALL['seg1'] = (sorted(glob(dirAligned + name + '*.segsection1')))
-    
 
-    # this is really stupid but i'm processing this per sample
-    for s in segSectionsALL:
-
-        dirSection_s = dirSection + s + "/"
-        dirMaker(dirSection_s)
-
-        print('Processing ' + s)
-        SegSections = segSectionsALL[s]
-        dirs = dictOfDirs(
-        tif = dataAlignedTif, 
-        SegSections = SegSections)
-
-        pos = list()
-        info = txtToDict(SegSections)
-
-        for n in info:
-            pos.append(abs(info[n][0]['tr'] - info[n][0]['bl']))
-
-        # get the maximum size of the selected section
-        xMax, yMax = np.array(np.max(pos, axis = 0))    
-
-        # ensure that there is a segSection availabe for all the images
-        for n in dirs:
-            try: 
-                dirs[n]['SegSections']
-                storeN = n      # store the last known instance where thre was a section annotated
-            except:
-                dirs[n]['SegSections'] = dirs[storeN]['SegSections']
+    segmentedSamples = dictOfDirs(
+        segments = glob(dirAligned + "*.tif"), 
+        segsection = glob(dirAligned + "*.segsection*"))
         
-        # parallelise jobs
-        jobs = {}
-        for n in dirs:
-            # segExtract(n, dirs[n], dirSection, yMax, xMax, s)
-            jobs[n] = (Process(target=segExtract, args=(n, dirs[n], dirSection_s, yMax, xMax, s)))     
-            jobs[n].start()
-        
-        for n in dirs:
-            jobs[n].join()
+    # if there is a missing segesction from the segmented samples, thenn propogate it  
+    # through the entire dictionary 
+    segmentedSamples, fields = standardiseSections(segmentedSamples, dirSection)
 
-def segExtract(n, dirs, dirSection, yMax, xMax, s):
+    # parallelise the extraction of the images
+    jobs = {}
+    for s in segmentedSamples:
+        jobs[s] = Process(target=segExtract, args = (segmentedSamples[s], dirSection, fields))
+        jobs[s].start()
+
+    for s in segmentedSamples:
+        jobs[s].join()
+
+
+def standardiseSections(segmentedSamples, dirSection):
+
+    # This function ensures that the features are propogated for all the samples
+    # Inputs:   (segmentedSamples), directory of directories which contains all the info
+    #           (dirSections), directory to create teh 
+
+    # identify all the sections types to find
+    segSectAll = list()
+    propogate = {}
+    for spec in np.flip(list(segmentedSamples.keys())):
+        for seg in segmentedSamples[spec]['segsection']:
+            if type(segmentedSamples[spec]['segsection']) is str:   # ensure processing the whole path
+                seg = segmentedSamples[spec]['segsection']
+            sample = seg.split(".")[-1]
+            propogate[sample] = seg
+            segSectAll.append(sample)
+    annotations = list(np.unique(np.array(segSectAll)))
+
+    # copy the necessary directories to the ones which are missing
+    for spec in segmentedSamples:
+        annotationsC = annotations.copy()
+        moveOn = False
+        for seg in segmentedSamples[spec]['segsection']:
+            if (type(segmentedSamples[spec]['segsection']) is str) & (moveOn == False):   # ensure processing the whole path
+                seg = segmentedSamples[spec]['segsection']
+                moveOn = True
+            elif moveOn:
+                break
+            sample = seg.split(".")[-1]
+            propogate[sample] = seg
+
+            # remove 
+            annotationsC.remove(seg.split(".")[-1])
+
+        # for the missing samples, propogate the last known one
+        for annoM in annotationsC:
+            if moveOn:
+                segmentedSamples[spec]['segsection'] = [segmentedSamples[spec]['segsection']]
+                moveOn = False
+            segmentedSamples[spec]['segsection'].append(propogate[annoM])
+
+    # create
+    segSections = {}
+    for a in annotations:
+        segSections[a] = {}
+        segSections[a]['bl'] = []
+        segSections[a]['tr'] = []
+
+    for spec in segmentedSamples:
+        for seg in segmentedSamples[spec]['segsection']:
+            # doesn't matter if its tr, tl etc will be made absolute... as long as its a diagnoal this will work
+            segSections[seg.split(".")[-1]]['bl'].append(txtToDict(seg)[0]['bl'])
+            segSections[seg.split(".")[-1]]['tr'].append(txtToDict(seg)[0]['tr'])
+
+    # create the uniform images sizes to copy each image to
+    fields = {}
+    for seg in segSections:
+        dirMaker(dirSection + seg + '/')    # make the directories for the images
+        xMax, yMax = np.array(np.max(abs(np.array(segSections[seg]['tr']) - np.array(segSections[seg]['bl'])), axis = 0))    
+        fields[seg] = np.zeros([yMax + 1, xMax + 1, 3]).astype(np.uint8)
+
+    return(segmentedSamples, fields)
+
+def segExtract(segment, dirSection, fields):
 
     # this function takes a single specified samples and the segmented feature of interest
     # and extracts it from the image
-    # Inputs:   (n), specimen of interest
-    #           (dirs), dictionary containing the directories of the specimen, NOTE because 
-    #           the point of this is to propogate a selected feature through the entire sample, 
-    #           if there is no specimen specicifc feature then it will use the last know selected feature
+    # Inputs:   (segment), dictionary of the specific sample
     #           (dirSection), the destiantion path to save the section
-    #           (yMax, xMax), the max size of the features being drawn to standardise the section size
-    #           (s), segment name
+    #           (fields), the arrays of standard size to save the info in
     # Outputs:  (), extracts from the aligned image the section selected and named after 
     #           the specimen name (n) and section name (s)           
 
-        
-    # create field to store all images in uniform shape
-    field = np.zeros([yMax + 1, xMax + 1, 3]).astype(np.uint8)
+    # load the image
+    img = tifi.imread(segment['segments'])
+    
+    # get the specimen name
+    name = nameFromPath(segment['segments'])
 
-    img = tifi.imread(dirs['tif'])
+    # for each segmentation annotation, extract and save it 
+    for segDir in segment['segsection']:
 
-    # use the selected image size
-    try:
-        pos = txtToDict(dirs['SegSections'])[0]
+        # get the sample name from the specific directory
+        sample = segDir.split(".")[-1]
+
+        # get the array for saving
+        field = fields[sample].copy()
+
+        # extract the section from the image
+        pos = txtToDict(segDir)[0]
         y0, x1 = pos['bl']
         y1, x0 = pos['tr']
+        imgSection = img[x0:x1, y0:y1, :]
+
+        # save the section 
+        x, y, c = imgSection.shape
+        field[:x, :y, :c] = imgSection
+        tifi.imwrite(dirSection + sample + '/' + name + "_" + sample + ".tif", field)
+
+        print(name + " " + sample + " extracted")
     
-    # if no image size is available then use the previously found position
-    except:
-        pass
 
-    imgSection = img[x0:x1, y0:y1, :]
-
-    # ensure all the saved images are the same size
-    x, y, c = imgSection.shape
-    field[:x, :y, :c] = imgSection
-    tifi.imwrite(dirSection + n + "_" + s + ".tif", field)
-
-    print("     " + n + " done")
-        
-
-
+dataTrain = '/Users/jonathanreshef/Documents/2020/Masters/TestingStuff/Segmentation/Data.nosync/HistologicalTraining2/'
 dataTrain = '/Volumes/Storage/H653A_11.3new/'
+
 name = ''
 size = 3
 
