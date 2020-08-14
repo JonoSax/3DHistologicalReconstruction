ccb624c2dac4f4ac4556b9301adc4b18a435c2c1
Jonathan Reshef <jono.reshef@gmail.com>
2020-08-14 21:19:49 +1200

    Seperated the GUI and co-ordinate extractor from the functions and created an Area and Point functionality

--- HelperFunctions/SP_SampleFinder.py
+++ HelperFunctions/SP_SampleFinder.py
@@ -13,11 +13,54 @@ import numpy as np
 # import tensorflow as tf
 from glob import glob
 import matplotlib.pyplot as plt
+import tifffile as tifi
+if __name__ == "__main__":
+    from Utilities import nameFromPath, dirMaker
+else:
+    from HelperFunctions.Utilities import nameFromPath, dirMaker
 
-def featSelect(imgref, imgtar, matchRef, matchTar, feats = 5):
+def featSelectArea(datahome, sample, feats = 5):
+
+    # this function brings up a gui which allows user to manually selection a 
+    # roi on the image. This extracts samples from the aligned tissues and saves them
+
+
+    segSections = datahome + "segSections/"
+
+    for f in range(feats):
+        dirMaker(segSections + "seg" + str(f) + "/")
+
+    alignedSamples = datahome + "alignedSamples/"
+
+    img = glob(alignedSamples + sample + "*.tif")[0]
+
+    samples = glob(alignedSamples + "*.tif")
+
+    if type(img) == str:
+        img = tifi.imread(img)
+
+    # extract n feats from the target samples
+    x = {}
+    y = {}
+    for f in range(feats):
+        x[f], y[f] = roiselector(img)
+        cv2.rectangle(img, (x[f][0], y[f][0]), (x[f][1], y[f][1]), (255, 255, 255), 40)
+        cv2.rectangle(img, (x[f][0], y[f][0]), (x[f][1], y[f][1]), (0, 0, 0), 20)
+
+    # extract from all the samples the features
+    for s in samples:
+        name = nameFromPath(s)
+        img = tifi.imread(s)
+        for f in range(feats):
+            segdir = segSections + "seg" + str(f) + "/"
+            section = img[y[f][0]:y[f][1], x[f][0]:x[f][1] :]
+            tifi.imwrite(segdir + name + ".tif", section)
+
+
+def featSelectPoint(imgref, imgtar, matchRef, matchTar, feats = 5):
 
     # this fuction brings up a gui which allows for a user to manually select
-    # features on the images
+    # features on the images. This contributes to making a .feat file
     # Inputs:   (nameref), either the path or the numpy array of the reference image
     #           (nametar), either the path or the numpy array of the target image
     #           (matchRef), any already identified features on the reference image
@@ -47,7 +90,6 @@ def featSelect(imgref, imgtar, matchRef, matchTar, feats = 5):
     imgCombine = np.hstack([img_refC, img_tarC])
     xc, yc, c = imgCombine.shape
 
-    rois = list()   # store all points
     n = 0           # iterator counter
 
     # draw on the already found points
@@ -56,6 +98,7 @@ def featSelect(imgref, imgtar, matchRef, matchTar, feats = 5):
         # enusre that the co-ordinate is in the right format and position 
         if type(r) is np.ndarray: r = tuple(r.astype(int))
         if type(t) is np.ndarray: t = tuple(t.astype(int) + np.array([ym, 0]))
+        else: t = tuple(np.array(t).astype(int) + np.array([ym, 0]))
         
         # get the ref and target points and adjust for the hstack
         # r = tuple(r.astype(int))
@@ -84,27 +127,16 @@ def featSelect(imgref, imgtar, matchRef, matchTar, feats = 5):
 
         n+=2    
 
-
-    # imgROI = cv2.imread(imgs[np.where(np.array([sample in i for i in imgs]) == True)[0][0]])
-
     # Have a UI to select features from a raw image
     # NOTE this image should be pre-selected and be an input into a function
-
     r = xc/yc
 
-    scale = xc/(1000 * r)
-
     for i in range(n, feats*2):
-        # perform a search over a reduced size area
-        roi = cv2.selectROI("image", cv2.resize(imgCombine, (1000, int(1000 * r))))
 
-        # get the postions
-        y = np.array([roi[1], roi[1] + roi[3]])
-        x = np.array([roi[0], roi[0] + roi[2]])
-
-        # scale the positions back to their original size
-        yme = int(np.mean(y) * scale)
-        xme = int(np.mean(x) * scale)
+        # get the x and y position from the feature
+        x, y = roiselector(imgCombine)
+        xme = int(np.mean(x))
+        yme = int(np.mean(y))
 
         # add the found points as marks
         imgCombine = cv2.circle(imgCombine, (xme, yme), 30, (255, 0, 0), 10)
@@ -119,8 +151,6 @@ def featSelect(imgref, imgtar, matchRef, matchTar, feats = 5):
             matchTar.append(np.array((xme, yme)) - np.array([ym, 0]))
             print("Feat: " + str(matchTar[-1]))
 
-        
-
         print(str(i) + " + " + obj)
 
         feat = obj + " feat " + str(int(np.floor(i/2)))
@@ -133,12 +163,35 @@ def featSelect(imgref, imgtar, matchRef, matchTar, feats = 5):
                 tuple([xme, yme] + np.array([20, 0])),
                 cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 0), 10)
 
-        rois.append(roi)
-
     cv2.destroyAllWindows()
 
-
     return(matchRef, matchTar)
+
+
+def roiselector(img):
+
+    # function which calls the gui and get the co-ordinates 
+    # Inputs    (img), numpy array image of interest
+    # Outputs   (xme, yme), x and y positions on the original image of the points selected
+    # ensure that the image is scaled to a resolution which can be used in the sceen
+    
+    xc, yc, c = img.shape
+    r = xc/yc
+    size = 700
+    scale = yc / (size / r)
+
+    # perform a search over a reduced size area
+    roi = cv2.selectROI("image", cv2.resize(img, (int(size / r), size)))
+
+    # get the postions
+    y = np.array([roi[1], roi[1] + roi[3]])
+    x = np.array([roi[0], roi[0] + roi[2]])
+
+    # scale the positions back to their original size
+    y = (np.round(y * scale)).astype(int)
+    x = (np.round(x * scale)).astype(int)
+
+    return(x, y)
 
 if __name__ == "__main__":
 
@@ -147,8 +200,13 @@ if __name__ == "__main__":
     matchRef = {}
     matchTar = {}
 
-    matchTar = [(4794, 2869), (4897, 3562), (4856, 958)]
-    matchRef = [(1928, 2865), (2040, 3536), (1949, 927)]
+    matchRef = [(1747, 2655), (1313, 1966), (649, 2438)]
+    matchTar = [(1776, 2662), (1348, 1950), (657, 2414)]
 
-    featSelect(nameref, nametar, matchRef, matchTar)
+    # featSelectPoint(nameref, nametar, matchRef, matchTar)
+
+    alignedimghome = '/Volumes/USB/H653/3/'
+    sample = 'H653_01A'
+
+    featSelectArea(alignedimghome, sample)
 
