9c5823bca4434f3357f06560373add357c36590f
Jonathan Reshef <jono.reshef@gmail.com>
2020-08-17 22:48:55 +1200

    updated for whole workflow

--- main_automaticAnnotations.py
+++ main_automaticAnnotations.py
@@ -14,15 +14,15 @@ Processing includes:
 from HelperFunctions import *
 
 # dataHome is where all the directories created for information are stored 
-dataTrain = '/Volumes/USB/H653/'
+dataHome = '/Volumes/USB/H653/'
 
-# dataTrain = '/Users/jonathanreshef/Documents/2020/Masters/TestingStuff/Segmentation/Data.nosync/HistologicalTraining2/'
+# dataHome = '/Users/jonathanreshef/Documents/2020/Masters/TestingStuff/Segmentation/Data.nosync/HistologicalTraining2/'
 
 # research drive access from HPC
-# dataTrain = '/eresearch/uterine/jres129/AllmaterialforBoydpaper/ResultsBoydpaper/ArcuatesandRadials/NDPIsegmentations/'
+# dataHome = '/eresearch/uterine/jres129/AllmaterialforBoydpaper/ResultsBoydpaper/ArcuatesandRadials/NDPIsegmentations/'
 
 # research drive access via VPN
-# dataTrain = '/Volumes/resabi201900003-uterine-vasculature-marsden135/All material for Boyd paper/Results Boyd paper/Arcuates and Radials/NDPI segmentations/'
+# dataHome = '/Volumes/resabi201900003-uterine-vasculature-marsden135/All material for Boyd paper/Results Boyd paper/Arcuates and Radials/NDPI segmentations/'
 
 
 size = 3
@@ -32,28 +32,28 @@ portion = 0.2
 
 print("\n----------- WSILoad -----------")
 # extract the tif file of the specified size
-WSILoad(dataTrain, name, size)
+WSILoad(dataHome, name, size)
 
 print("\n----------- smallerTif -----------")
 # create jpeg images of all the tifs and a single collated pdf
-smallerTif(dataTrain, name, size)
+smallerTif(dataHome, name, size, 0.2)
 
 print("\n----------- specID -----------")
 # identifies the sample within the slide from the jpegs created
-specID(dataTrain, name, size)
+specID(dataHome, name, size)
 
 print("\n----------- featFind -----------")
 # identifies corresponding features per samples 
-featFind(dataTrain, name, size)
+featFind(dataHome, name, size)
 
 print("\n----------- SegmentExtract -----------") # --> NOTE add size of all the shapes and make these functions adjust the size of the files for this image size
 # extract only the sample from the whole slide
 # if there are multiple specimens, seperate these into a, b, c... samples (including info)
-# sampleExtract(dataTrain, name, size, True)
+# sampleExtract(dataHome, name, size, True)
 
 print("\n----------- AignSegments -----------") # --> NOTE add size of all the shapes and make these functions adjust the size of the files for this image size
 # align all the specimens 
-align(dataTrain, name, size, True)
+align(dataHome, name, size, True)
 
 # NOTE to complete
 # Identify a feature to propogate through the entire tissue
@@ -61,7 +61,7 @@ align(dataTrain, name, size, True)
 
 print("\n----------- FeatureExtraction -----------")
 # propogate an annotated feature through the aligned tissue and extract
-# featExtract(dataTrain, name, size)
+featSelectArea(dataHome, size, 'H653_01A', 3)
 
 # NOTE run the results of the feat extraction THROUGH the align function again to 
 # provide the fine scale alignment for the tissue segment
