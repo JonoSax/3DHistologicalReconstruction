da4ef69f94afdabff93c9d8ee67550b169aa729e
Jonathan Reshef <jono.reshef@gmail.com>
2020-08-18 16:56:12 +1200

    major clean up and made it more modular to allow for parallelising if that's something worth doing

--- HelperFunctions/SP_tif2pdf.py
+++ HelperFunctions/SP_tif2pdf.py
@@ -22,21 +22,20 @@ def smallerTif(dataHome, name, size, scale = 0.3):
     # create a pdf file from all of them
 
     files = sorted(glob(dataHome + str(size) + "/tifFiles/*.tif"))
-    allSamples = {}
-    allSamples[nameFromPath(files[0], 1)] = {}
+    allSpecimens = {}
+    allSpecimens[nameFromPath(files[0], 1)] = {}
 
     path = dataHome + str(size) + "/"
 
     # this is creating a dictionary of all the sample paths per specimen
     for n in files:
-        allSamples[nameFromPath(files[0], 1)][nameFromPath(n).split("_")[-1]] = n
+        allSpecimens[nameFromPath(files[0], 1)][nameFromPath(n).split("_")[-1]] = n
 
     # get a dictionary of all the sample to process
-    # allSamples = sampleCollector(dataHome, size)
+    # allSpecimens = sampleCollector(dataHome, size)
 
-
-    for spec in allSamples:
-        pdfCreator(allSamples[spec], spec, path, scale, False)
+    for spec in allSpecimens:
+        pdfCreator(allSpecimens[spec], spec, path, scale, False)
 
 def pdfCreator(specificSample, spec, path, scale, remove = True):
     # this function takes the directory names and creates a pdf of each sample 
@@ -65,55 +64,30 @@ def pdfCreator(specificSample, spec, path, scale, remove = True):
 
     # create an ordered list of the sample directories
     c = 0   # count for user to observe progress
-
     allShape = {}
     startTime = clock()
-    for n in order:
 
-        print("Specimen: " + spec + ", Sample " + str(c) + "/" + str(len(order)))
-        # load in the tif files and create a scaled down version
-        imgt = tifi.imread(specificSample[n])
+    # NOTE creating the jpeg files should be very parallelised and at the end
+    # the order of the files shoudl be sorted out and the pdf created
+    for n, name in zip(order, nameFromPath(specificSample)):
+        allShape[name] = miniSample(dataTemp, specificSample[n], scale, n)
 
-        # save the shape of the original tif image
-        allShape[nameFromPath(specificSample[n])] = imgt.shape
-
-        # scale = imgt.shape[1]/imgt.shape[0]
-        # img = cv2.resize(imgt, (int(1000*scale), 1000))
-        img = cv2.resize(imgt, (int(imgt.shape[1] * scale),  int(imgt.shape[0] * scale)))
-
-        # NOTE right here could be a SINGLE function which takes the info and 
-        # determines if there are any hard coded rules to apply
-        # for sample H710C, all the c samples are rotated
-        if (n.lower().find("c") >= 0) & (spec.lower().find("h710c") >= 0):
-            img = cv2.rotate(img, cv2.ROTATE_180)
-
-        # add the sample name to the image (top left corner)
-        cv2.putText(img, spec + "_" + str(n), 
-            (50, 50), 
-            cv2.FONT_HERSHEY_SIMPLEX, 
-            1,
-            (0, 0, 0),
-            2)
-        # create a temporary jpg image and store the dir
-        tempName = dataTemp + spec + "_" + str(n) + '.png'
-        cv2.imwrite(tempName, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        # tifi.imwrite(tempName, img)
-        dirStore.append(tempName)
-        c += 1
-
-        # provide some timing information
-        endTime = clock()
-        timeOfProcess = endTime - startTime
-        timeLeft = timeOfProcess/c * (len(order) - c)
-        if c%5 == 0:
-            print("     Sample " + spec + " has " + str(timeLeft) +  " secs to go")
-    
     # create the all.shape information file
     dictToTxt(allShape, path + "info/all.shape")
+    
+    # NOTE the order should be the same as the tif files because it is collected 
+    # with glob in the same way and the sample name is preserved (?? verify...)
+    # Just FYI on why this is so convoluted, it is because the file names can often
+    # be stupid names so there is bit of a process for getting them right!
+    dirStore = sorted(glob(dataTemp + spec + "*.png"))
+    allsmallSamples = {}
+    for n in dirStore:
+        allsmallSamples[nameFromPath(n).split("_")[-1]] = n
 
-    # comment the above loop and uncomment below to process already processed images
-    # without having to re-process all images
-    # dirStore = sorted(glob(dataTemp + spec + "*.jpg"))
+    # create a list of ordered directories to create the pdf
+    dirStore = []
+    for n in order:
+        dirStore.append(allsmallSamples[n])
 
     # combine all the sample images to create a single pdf 
     with open(dataPDF + spec + "NotScaled.pdf","wb") as f:
@@ -129,6 +103,50 @@ def pdfCreator(specificSample, spec, path, scale, remove = True):
         print("Removing " + dataTemp)
 
     # research drive access via VPN
+
+def miniSample(dataTemp, sample, scale, n):
+
+    # this function loads a single image, downsizes it and annotates it 
+    # Inputs:   (dataTemp), directory to store the images in
+    #           (sample), the directory of the tif file
+    #           (scale), the factor to downscale the image
+    #           (n), sample categorical name
+    # Outputs:  (), saves a lower res image of the tif file with spec info on it
+    #           (img.shape), returns the downsampled img shape
+
+    # load in the tif files and create a scaled down version
+    # imgt = tifi.imread(specificSample[n])
+
+    spec = nameFromPath(sample) 
+    imgt = tifi.imread(sample)
+
+    print("Specimen: " + spec + " downsampled")
+
+    # scale = imgt.shape[1]/imgt.shape[0]
+    # img = cv2.resize(imgt, (int(1000*scale), 1000))
+    img = cv2.resize(imgt, (int(imgt.shape[1] * scale),  int(imgt.shape[0] * scale)))
+
+    # NOTE right here could be a SINGLE function which takes the info and 
+    # determines if there are any hard coded rules to apply
+    # for sample H710C, all the c samples are rotated
+    if (n.lower().find("c") >= 0) & (spec.lower().find("h710c") >= 0):
+        img = cv2.rotate(img, cv2.ROTATE_180)
+
+    # add the sample name to the image (top left corner)
+    cv2.putText(img, spec + "_" + str(n), 
+        (50, 50), 
+        cv2.FONT_HERSHEY_SIMPLEX, 
+        1,
+        (0, 0, 0),
+        2)
+    # create a temporary jpg image and store the dir
+    tempName = dataTemp + spec + '.png'
+    cv2.imwrite(tempName, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # tifi.imwrite(tempName, img)
+
+    return(imgt.shape)
+
+
 
 def sampleCollector(dataHome, size):
 
