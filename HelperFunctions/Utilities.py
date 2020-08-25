10c0bfe62460d9b514ef8dcff617bcfef9850c0c
Jonathan Reshef <jono.reshef@gmail.com>
2020-08-25 23:22:19 +1200

    added hist match

--- HelperFunctions/Utilities.py
+++ HelperFunctions/Utilities.py
@@ -535,3 +535,55 @@ def extractFeatureInfo(featInfo, feat):
         
 
     return(specFeatInfo)
+
+def hist_match(source, template):
+    """
+    Courtesy of https://stackoverflow.com/questions/31490167/how-can-i-transform-the-histograms-of-grayscale-images-to-enforce-a-particular-r/31493356#31493356
+    Adjust the pixel values of a grayscale image such that its histogram
+    matches that of a target image
+
+    Arguments:
+    -----------
+        source: np.ndarray
+            Image to transform; the histogram is computed over the flattened
+            array
+        template: np.ndarray
+            Template image; can have different dimensions to source
+    Returns:
+    -----------
+        matched: np.ndarray
+            The transformed output image
+    """
+
+    # NOTE this is done here rather than in SpecimenID because it only works well
+    # when the sample is very well identified
+
+    oldshape = source.shape
+    source = source.ravel()
+    template = template.ravel()
+
+    # get the set of unique pixel values and their corresponding indices and
+    # counts
+    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                            return_counts=True)
+    t_values, t_counts = np.unique(template, return_counts=True)
+
+    # remove the effect of black (it is working on a masked image)
+    s_counts[0] = 0
+    t_counts[0] = 0
+
+    # take the cumsum of the counts and normalize by the number of pixels to
+    # get the empirical cumulative distribution functions for the source and
+    # template images (maps pixel value --> quantile)
+    s_quantiles = np.cumsum(s_counts).astype(np.float64)
+    s_quantiles /= s_quantiles[-1]
+    t_quantiles = np.cumsum(t_counts).astype(np.float64)
+    t_quantiles /= t_quantiles[-1]
+
+    # interpolate linearly to find the pixel values in the template image
+    # that correspond most closely to the quantiles in the source image
+    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+    # return the image with normalised distribtution of pixel values and 
+    # in the same data type as np.uin8
+    return (interp_t_values[bin_idx].reshape(oldshape)).astype(np.uint8)
