371339ebad0167e355945f3c242afbe31cfda3fc
Jonathan Reshef <jonathanreshef@Jonathans-MacBook-Pro.local>
2020-09-22 09:12:47 +1200

    updated functions

--- HelperFunctions/__init__.py
+++ HelperFunctions/__init__.py
@@ -1,12 +1,11 @@
 from .PR_SegmentLoad import SegLoad
 from .PR_WSILoad import WSILoad
 from .SP_MaskMaker import maskMaker
-# from .SP_SampleFinder import 
-from .SP_SingleSegmentExtract import sampleExtract
 from .SP_AlignSamples import align
 from .SP_tif2pdf import smallerTif
 from .SP_SpecimenID import specID
 from .SP_FeatureFinder import featFind
 from .SP_SampleAnnotator import featSelectArea, featChangePoint
+from .fixSample import fixit
 from .CI_WSIExtract import WSIExtract
-from . CI_alignfeatSelect import fullMatchingSpec   
+from .CI_alignfeatSelect import fullMatchingSpec   
