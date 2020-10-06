ad412f5dae463563b418396a6f4c8af936ca2e0b
Jonathan Reshef <jonathanreshef@Jonathans-MacBook-Pro.local>
2020-10-06 14:07:19 +1300

    updated

--- HelperFunctions/__init__.py
+++ HelperFunctions/__init__.py
@@ -1,11 +1,12 @@
 from .PR_SegmentLoad import SegLoad
 from .PR_WSILoad import WSILoad
-from .SP_MaskMaker import maskMaker
 from .SP_AlignSamples import align
-from .SP_tif2pdf import smallerTif
-from .SP_SpecimenID import specID
 from .SP_FeatureFinder import featFind
+from .SP_MaskMaker import maskMaker
 from .SP_SampleAnnotator import featSelectArea, featChangePoint
+from .SP_SpecimenID import specID
+from .SP_tif2pdf import smallerTif
 from .fixSample import fixit
-from .CI_WSIExtract import WSIExtract
 from .CI_alignfeatSelect import fullMatchingSpec   
+from .CI_targetTissue import targetTissue
+from .CI_WSIExtract import WSIExtract
