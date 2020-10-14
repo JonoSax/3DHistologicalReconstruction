bf6bcf580226eaee4cd9f8236a0dc6de26fe5e0f
Jonathan Reshef <jonathanreshef@Jonathans-MacBook-Pro.local>
2020-10-14 15:53:34 +1300

    more speciifc to downsampling images than tif2pdf

---
+++ HelperFunctions/SP_smallSample.py
@@ -0,0 +1,61 @@
+import tifffile as tifi
+import cv2 
+from glob import glob
+import multiprocessing
+from multiprocessing import Pool
+from itertools import repeat
+if __name__ != "HelperFunctions.SP_AlignSamples":
+    from HelperFunctions.Utilities import nameFromPath, dirMaker
+else:
+    from Utilities import nameFromPath, dirMaker
+
+def downsize(dataHome, size, scale = 0.3, cpuNo = False):
+
+    # downsize the tif images, move them into the images directory and 
+    # rename as appropriate
+
+    files = sorted(glob(dataHome + str(size) + "/tifFiles/*.tif"))
+
+    targetDir = dataHome + str(size) + "/images/"
+    dirMaker(targetDir)
+
+    if cpuNo is False:
+        for f in files:
+            ds(f, scale, targetDir)
+    else:
+        with Pool(processes=cpuNo) as pool:
+            pool.starmap(ds, zip(files, repeat(scale), repeat(targetDir)))
+
+def ds(f, scale, targetDir):
+
+    sampName = nameFromPath(f)
+    tarPath = targetDir + sampName + ".png"
+
+    print(sampName + " being resized")
+
+    # read in the image 
+    img = tifi.imread(f)
+
+    # resize the image
+    img = cv2.resize(img, (int(img.shape[1] * scale),  int(img.shape[0] * scale)))
+
+    # save the image as a png
+    cv2.imwrite(tarPath, img)
+
+if __name__ == "__main__":
+
+    multiprocessing.set_start_method("spawn")
+
+    dataSource = '/Volumes/USB/H673A_7.6/'
+    dataSource = '/Volumes/USB/H671A_18.5/'
+    dataSource = '/Volumes/Storage/H653A_11.3/'
+
+    dataSource = '/Volumes/USB/Test/'
+
+
+    size = 3
+    name = ''
+    scale = 0.2
+    cpuNo = 6
+
+    downsize(dataSource, size, scale, cpuNo)
