e6b3d99835e094df247b3a6432f5341c179653af
Jonathan Reshef <jono.reshef@gmail.com>
2020-12-03 16:21:16 +1300

    improved parallelisation

--- pullTifFilesFromHPC.py
+++ pullTifFilesFromHPC.py
@@ -1,10 +1,11 @@
 from glob import glob
 from HelperFunctions.Utilities import nameFromPath, dirMaker
 import os
+import multiprocessing
 from multiprocessing import Process
 
 dataHome = '/Volumes/resabi201900003-uterine-vasculature-marsden135/BoydCollection/'
-samples = ['H1029A_8.4', 'H653A_11.3', 'H671A_18.5', 'H671B_18.5', 'H673A_7.6', 'H710B_6.1', 'H710C_6.1', 'H750A_7.0' ]
+samples = ['H653A_11.3']#['H1029A_8.4', 'H653A_11.3', 'H671A_18.5', 'H671B_18.5', 'H673A_7.6', 'H710B_6.1', 'H710C_6.1', 'H750A_7.0' ]
 
 
 def getFiles(s, dataHome):
@@ -14,10 +15,24 @@ def getFiles(s, dataHome):
     print("Finished " + s)
 
 
-jobs = {}
-for s in samples:
-    jobs[s] = Process(target = getFiles, args = (s, dataHome))
-    jobs[s].start()
 
-for s in samples:
-    jobs[s].join()
+
+if __name__ == "__main__":
+
+    multiprocessing.set_start_method("fork")
+
+    cpuNo = 1
+
+    if cpuNo != 1:
+
+        jobs = {}
+        for s in samples:
+            jobs[s] = Process(target = getFiles, args = (s, dataHome))
+            jobs[s].start()
+
+        for s in samples:
+            jobs[s].join()
+
+    else:
+        for s in samples:
+            getFiles(s, dataHome)
