4600b376776606c7da3ecd459353c9f7f0da4660
Jonathan Reshef <jono.reshef@gmail.com>
2021-01-28 10:18:41 +1300

    some improved error handling

--- HelperFunctions/SP_smallSample.py
+++ HelperFunctions/SP_smallSample.py
@@ -7,7 +7,7 @@ from itertools import repeat
 if __name__ != "HelperFunctions.SP_AlignSamples":
     from HelperFunctions.Utilities import nameFromPath, dirMaker
 else:
-    from Utilities import nameFromPath, dirMaker
+    from HelperFunctions.Utilities import nameFromPath, dirMaker
 
 def downsize(dataHome, size, scale = 0.3, cpuNo = False):
 
@@ -36,15 +36,17 @@ def ds(f, scale, targetDir):
     tarPath = targetDir + sampName + ".png"
 
     print(sampName + " being resized")
+    try:
+        # read in the image 
+        img = cv2.cvtColor(tifi.imread(f), cv2.COLOR_BGR2RGB)
 
-    # read in the image 
-    img = cv2.cvtColor(tifi.imread(f), cv2.COLOR_BGR2RGB)
+        # resize the image
+        img = cv2.resize(img, (int(img.shape[1] * scale),  int(img.shape[0] * scale)))
 
-    # resize the image
-    img = cv2.resize(img, (int(img.shape[1] * scale),  int(img.shape[0] * scale)))
-
-    # save the image as a png
-    cv2.imwrite(tarPath, img)
+        # save the image as a png
+        cv2.imwrite(tarPath, img)
+    except:
+        print("     " + sampName + " FAILED")
 
 if __name__ == "__main__":
 
