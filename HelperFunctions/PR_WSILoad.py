102ccca5388e765f1c26aa50f1599b50984898e3
Jonathan Reshef <jono.reshef@gmail.com>
2021-02-19 17:29:37 +1300

    added parallelising and better naming

--- HelperFunctions/PR_WSILoad.py
+++ HelperFunctions/PR_WSILoad.py
@@ -4,8 +4,9 @@ This function extracts a tif file of specified resolution from the ndpi file
 
 import os
 from glob import glob
-from multiprocessing import Process
-if __name__ == "__main__":
+from multiprocessing import Pool
+from itertools import repeat
+if __name__ != "HelperFunctions.PR_WSILoad":
     from Utilities import *
 else:
     from HelperFunctions.Utilities import *
@@ -17,10 +18,10 @@ else:
 # magnification levels of the tif files available
 tifLevels = [20, 10, 5, 2.5, 1.25, 0.625, 0.3125, 0.15625]
 
-def WSILoad(dataTrain, name, size):
+def WSILoad(dataTrain, name, size, cpuNo = False):
 
     # this is the function called by main. Organises the inputs for findFeats
-    ndpis = sorted(glob(dataTrain + name + "*.*"))
+    ndpis = sorted(glob(dataTrain + name + "*.nd*"))
 
     # ensure all ndpi files have at least a two digit category number (prevents 
     # false identification)
@@ -31,11 +32,12 @@ def WSILoad(dataTrain, name, size):
         prefix = "." + ndpi.split(".")[-1]
 
         # if there are less than two digits in the name, add a 0
-        if len(no[-1]) == 1:
-            name = no[0] + "_00" + no[-1]
-        elif len(no[-1]) == 2:
-            name = no[0] + "_0" + no[-1]
+        while len(no[-1]) < 4:
+            no[-1] = "0" + no[-1]
 
+        name = no[0] + "_" + no[-1]
+
+        # if the name has been modified rename it
         if ndpi.replace(" ", "\ ") != home + name + prefix:
             os.system("mv " + ndpi.replace(" ", "\ ") + " " + home + name + prefix)
         else:
@@ -44,6 +46,14 @@ def WSILoad(dataTrain, name, size):
     specimens = sorted(glob(dataTrain + "*.*"))
     # load(dataTrain, '', size)
 
+    if cpuNo > 1: 
+        with Pool(processes = cpuNo) as pool: 
+            pool.starmap(load, zip(specimens, repeat(dataTrain), repeat(size)))
+    else:
+        for s in specimens:
+            load(s, dataTrain, size)
+
+    '''
     # parallelise work
     jobs = {}
 
@@ -54,8 +64,9 @@ def WSILoad(dataTrain, name, size):
     for spec in specimens:
         jobs[spec].join()
     
+    '''
 
-def load(dataTrain, dirimg = '', size = 0):
+def load(dirimg, dataTrain, size = 0):
 
     # This moves the quadrants into training/testing data based on the annotations provided
     # Input:    (dataTrain), directory/ies which contain the txt files of the annotation co-ordinates 
@@ -76,20 +87,18 @@ def load(dataTrain, dirimg = '', size = 0):
     
 
     ndpiLoad(size, dirimg, dataTif)
-    print(nameFromPath(dirimg) + " converted @ " + str(tifLevels[size]) + " res")
+    print(nameFromPath(dirimg) + " converted @ " + str(size) + " res")
 
-def ndpiLoad(sz, src, dest):
+def ndpiLoad(mag, src, dest):
 
     # This function extracts tif files from the raw ndpi files. This uses the 
     # ndpitool from https://www.imnc.in2p3.fr/pagesperso/deroulers/software/ndpitools/ 
     # Install as necessary. 
-    # Input:    (i), magnificataion level to be extracted from the ndpi file
+    # Input:    (mag), magnificataion level to be extracted from the ndpi file
     #           options are 0.15625, 0.3125, 0.625, 1.25, 2.5, 5, 10, 20
     #           (src), file to be extracted with set magnification
     #           (dest), destination
     # Output:   (), tif file of set magnification, saved in the tifFiles directory
-
-    mag = tifLevels[sz]
 
     # nameSRC = src.split("/")[-1].split(".")[0]                    # photo name
     nameSRC = nameFromPath(src)
@@ -107,16 +116,17 @@ def ndpiLoad(sz, src, dest):
                                                                 # duplication of the same file, however 
                                                                 # if there is z shift then this will fail
 
-    imgDir = dest + nameSRC + "_" + str(sz) + ".tif"
+    imgDir = dest + nameSRC + "_" + str(mag) + ".tif"
     os.rename(extractedName, imgDir)
 
     
 if __name__ == "__main__":
 
-    dataTrain = '/Volumes/USB/H653/'
     dataTrain = '/Volumes/USB/IndividualImages/temporaryH653/'
+    dataTrain = '/Volumes/USB/Testing/'
     name = ''
-    size = 3
+    size = 5
+    cpuNo = 1
 
-    WSILoad(dataTrain, name, size)
+    WSILoad(dataTrain, name, size, cpuNo)
     
