818a337507145f7dce4b628d331aae329017b617
Jonathan Reshef <jono.reshef@gmail.com>
2021-02-22 10:40:58 +1300

    now rotates samples and creates a _rotatedSamples file to check before rotating

--- HelperFunctions/SP_smallSample.py
+++ HelperFunctions/SP_smallSample.py
@@ -4,8 +4,11 @@ from glob import glob
 import multiprocessing
 from multiprocessing import Pool
 from itertools import repeat
+import numpy as np
+
+from tifffile.tifffile import imagej_description
 if __name__ != "HelperFunctions.SP_AlignSamples":
-    from HelperFunctions.Utilities import nameFromPath, dirMaker
+    from Utilities import nameFromPath, dirMaker
 else:
     from HelperFunctions.Utilities import nameFromPath, dirMaker
 
@@ -14,7 +17,9 @@ def downsize(dataHome, size, scale = 0.3, cpuNo = False):
     # downsize the tif images, move them into the images directory and 
     # rename as appropriate
 
-    files = sorted(glob(dataHome + str(size) + "/tifFiles/*.tif"))
+    tifFiles = dataHome + str(size) + "/tifFiles/"
+
+    files = sorted(glob(tifFiles + "*.tif"))
 
     # create an error if there arent enough images
     if len(files) < 2:
@@ -23,30 +28,58 @@ def downsize(dataHome, size, scale = 0.3, cpuNo = False):
     targetDir = dataHome + str(size) + "/images/"
     dirMaker(targetDir)
 
-    if cpuNo is False:
+    unrotated = len(glob(tifFiles + "_rotatedImgs*")) != 1
+
+    imgsRotated = []
+    if cpuNo == 1:
         for f in files:
-            ds(f, scale, targetDir)
+            imgsRotated.append(ds(f, scale, targetDir, unrotated))
     else:
         with Pool(processes=cpuNo) as pool:
-            pool.starmap(ds, zip(files, repeat(scale), repeat(targetDir)))
+            imgsRotated = pool.starmap(ds, zip(files, repeat(scale), repeat(targetDir)))
+            
+    f = open(tifFiles + "_rotatedImgs.txt", "w")
+    for i in imgsRotated:
+        f.write(i + "\n")
+    f.close()
 
-def ds(f, scale, targetDir):
+def ds(f, scale, targetDir, unrotated = False):
 
     sampName = nameFromPath(f)
     tarPath = targetDir + sampName + ".png"
 
+    name, id = sampName.split("_")
+
+    # if samples with c are present, rotate
+    if name.find("H710C") > -1 and id.find("C") > -1: 
+        rotate = np.array([unrotated, True]).all()
+    else:   rotate = False
+
     print(sampName + " being resized")
     try:
-        # read in the image 
-        img = cv2.cvtColor(tifi.imread(f), cv2.COLOR_BGR2RGB)
+
+        imgF = tifi.imread(f)
 
         # resize the image
-        img = cv2.resize(img, (int(img.shape[1] * scale),  int(img.shape[0] * scale)))
+        img = cv2.resize(imgF, (int(imgF.shape[1] * scale),  int(imgF.shape[0] * scale)))
+
+        # convert the colour for proper saving 
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # rotate if true
+        if rotate:  
+            print("     ROTATING " + sampName)
+            img = cv2.rotate(img, cv2.ROTATE_180)
+            imgF = cv2.rotate(imgF, cv2.ROTATE_180)
+            tifi.imwrite(f, imgF)
 
         # save the image as a png
         cv2.imwrite(tarPath, img)
+
+        return(sampName + "_" + str(rotate))
     except:
         print("     " + sampName + " FAILED")
+        return(sampName + "_FAILED")
 
 if __name__ == "__main__":
 
@@ -54,14 +87,16 @@ if __name__ == "__main__":
 
     dataSource = '/Volumes/USB/H673A_7.6/'
     dataSource = '/Volumes/USB/H671A_18.5/'
-    dataSource = '/Volumes/Storage/H653A_11.3/'
 
     dataSource = '/Volumes/USB/Test/'
+    dataSource = '/Volumes/USB/H653A_11.3/'
+    dataSource = '/Volumes/USB/H710C_6.1/'
+
 
 
     size = 3
     name = ''
     scale = 0.2
-    cpuNo = 6
+    cpuNo = 1
 
     downsize(dataSource, size, scale, cpuNo)
