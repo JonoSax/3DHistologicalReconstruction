d448ac05a09565bf819669fb226a787c79e4f581
Jonathan Reshef <jono.reshef@gmail.com>
2021-05-06 17:25:09 +1200

    fully parallelised and works

--- main_fullScaleTransform.py
+++ main_fullScaleTransform.py
@@ -3,27 +3,29 @@ This script takes all the modifications that were performed on the baseline imag
 and applies them to the full-scale tif images
 '''
 
-from nonRigidAlign import nonRigidAlign, nonRigidDeform
-from HelperFunctions.Utilities import nameFromPath, txtToDict
+from HelperFunctions.nonRigidAlign import nonRigidDeform
+from HelperFunctions.Utilities import getSampleName, exactBound
 from HelperFunctions.SP_AlignSamples import getSpecShift, transformSamples
 from HelperFunctions.SP_SpecimenID import imgStandardiser
-import multiprocessing
-from HelperFunctions import *
+from multiprocessing import Pool
 from glob import glob
 import cv2
-import tifffile as tifi
+from itertools import repeat
 
 if __name__ == "__main__":
 
-    size = 3
+    size = 1.25
+    res = 0.4
+    scale = 1/res
+    cpuNo = 4
 
     dataHomes = [
-    '/Volumes/USB/H653A_11.3/',
+    # '/Volumes/USB/H653A_11.3/',
     # '/Volumes/USB/H671A_18.5/',
     # '/Volumes/USB/H671B_18.5/',
     # '/Volumes/USB/H673A_7.6/',
     # '/Volumes/USB/H710B_6.1/',
-    # '/Volumes/USB/H710C_6.1/',
+    '/Volumes/USB/H710C_6.1/',
     # '/Volumes/USB/H750A_7.0/',
     # '/Volumes/USB/H1029A_8.4/'
     ]
@@ -54,49 +56,32 @@ if __name__ == "__main__":
         RealignedSamples = datasrc + "fullScaleReAlignedSamples/"
         featureStore = datasrc + "FeatureSections/"
         NLAlignedSamples = datasrc + "fullScaleNLAlignedSamples/"
-
+        NLAlignedSamplesBound = datasrc + "/fullScaleNLAlignedSamplesBound/"
         
-        '''
         # apply the masks onto the tif images and normalise the colours
-        imgref = cv2.imread(d + "refimg.png")
+        print("------ Masking ------")
+        imgref = cv2.imread(getSampleName(d, "refimg.png"))
         imgMasked = datasrc + "maskedSamples/masks/"
         masks = sorted(glob(imgMasked + "*.pbm"))
-        for m in masks:
-            imgStandardiser(imgMaskedSamples, m, imgsrc, imgref)
-        '''
         
+        with Pool(processes = cpuNo) as pool:
+            pool.starmap(imgStandardiser, zip(repeat(imgMaskedSamples), masks, repeat(imgsrc), repeat(imgref), repeat(scale)))
+
+        print("------ Linear regisration ------")
         # perform the first iteration of the aligned samples
         maxShape, minShift = getSpecShift(featureInfoPath)
         samples = sorted(glob(imgMaskedSamples + "*.tif"))
-        for sample in samples:
-            transformSamples(sample, maxShape, minShift, featureInfoPath, alignedSamples, False)
-
-        # bound the images the SAME as the baseline aligned images
-        bound = txtToDict(baselineAlignedSample + "_boundingPoints.txt", int)
-        imgs = glob(alignedSamples + "*.tif")
-        x, y = bound
-        for i in imgs:
-            name = nameFromPath(i)
-            img = cv2.imread(i)
-            imgB = img[y[0]:y[1], x[0]:x[1], :]
-            tifi.imwrite(i, imgB)
+        with Pool(processes = cpuNo) as pool:
+            pool.starmap(transformSamples, zip(samples, repeat(maxShape), repeat(minShift), repeat(featureInfoPath), repeat(alignedSamples), repeat(False), repeat(2.5)))
 
         # perform the re-linear alignment with the NL features
+        print("------ Linear re-registration ------")
         maxShape, minShift = getSpecShift(NLfeatureInfoPath)
         samples = sorted(glob(alignedSamples + "*.tif"))
-        for sample in samples:
-            transformSamples(sample, maxShape, minShift, featureInfoPath, RealignedSamples, False)
-
-        # bound the images the SAME as the baseline realigned images
-        bound = txtToDict(baselineReAlignedSample + "_boundingPoints.txt", int)
-        imgs = glob(RealignedSamples + "*.tif")
-        x, y = bound
-        for i in imgs:
-            name = nameFromPath(i)
-            img = cv2.imread(i)
-            imgB = img[y[0]:y[1], x[0]:x[1], :]
-            tifi.imwrite(i, imgB)
+        with Pool(processes = cpuNo) as pool:
+            pool.starmap(transformSamples, zip(samples, repeat(maxShape), repeat(minShift), repeat(NLfeatureInfoPath), repeat(RealignedSamples), repeat(False), repeat(2.5)))
 
         # perform NL warping
-        nonRigidDeform(RealignedSamples, NLAlignedSamples, featureStore, scl = 1, prefix = "tif")
-
+        print("------ NL registration ------")
+        nonRigidDeform(RealignedSamples, NLAlignedSamples, featureStore, scl = scale, prefix = "tif")
+        exactBound(NLAlignedSamples, "tif", dest = NLAlignedSamplesBound)
